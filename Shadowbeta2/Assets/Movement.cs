722c96f69b02f290e709f55e63e6729b54569e54
BeauTaapken <beautaapken@me.com>
2019-12-05 13:45:44 +0100

    * Changed script * Added lighting

--- Shadowbeta2/Assets/Movement.cs
+++ Shadowbeta2/Assets/Movement.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public float walkingSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +14,16 @@ public class Movement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        Vector2 movement = new Vector2(horizontal, 0.0f);
+
+        //Character movement on x and z axis
+        if (!Mathf.Approximately(movement.x, 0.0f))
+        {
+            transform.Translate(walkingSpeed * movement.normalized * Time.deltaTime, Space.World);
+        }
     }
 }
