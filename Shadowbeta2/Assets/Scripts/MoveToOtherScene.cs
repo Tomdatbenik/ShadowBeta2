5ef940f0f633ff26a48bdfdb8840c17ccee7cc1b
BeauTaapken <beautaapken@me.com>
2019-12-05 14:39:31 +0100

    * Added script to load other scenes

---
+++ Shadowbeta2/Assets/Scripts/MoveToOtherScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveToOtherScene : MonoBehaviour
+{
+    public Object transportScene;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        float interact = Input.GetAxisRaw("Interact");
+        if (Mathf.Approximately(interact, 1))
+        {
+            SceneManager.LoadScene(transportScene.name);
+        }
+    }
+}
