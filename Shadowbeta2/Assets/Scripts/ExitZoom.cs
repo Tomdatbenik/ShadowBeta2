340943ecd2dd7ec5dd60d00240d64a14408339e6
BeauTaapken <beautaapken@me.com>
2019-12-06 13:05:51 +0100

    * Key is now esc in pc screen

--- Shadowbeta2/Assets/Scripts/ExitZoom.cs
+++ Shadowbeta2/Assets/Scripts/ExitZoom.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class ExitZoom : MonoBehaviour
 {
     public Object sceneToLoad;
+    public Animator animator;
 
     // Update is called once per frame
     void Update()
     {
+        animator.SetBool("isExit", true);
         float exit = Input.GetAxis("Cancel");
         if (Mathf.Approximately(exit, 1))
         {
