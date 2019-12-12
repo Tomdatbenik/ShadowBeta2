d45629a953799233d11f4e59cf0c7011ddcae775
BeauTaapken <beautaapken@me.com>
2019-12-12 14:33:58 +0100

    * Added drag and drop * Added animation to hackbox

---
+++ Shadowbeta2/Assets/Scripts/DragAndDrop/DragAndDrop.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private bool dragging = false;
+    private float distance;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    Debug.Log(dragging);
+    //}
+
+    //private void OnMouseDown()
+    //{
+    //    dragging = true;
+    //}
+
+    //private void OnMouseUp()
+    //{
+    //    dragging = false;
+    //}
+
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+    }
+
+    void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint;
+        }
+    }
+}
