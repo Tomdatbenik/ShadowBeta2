67d207bd6bfb2a95f04feb4c728e411aec5b00b1
BeauTaapken <beautaapken@me.com>
2019-12-12 15:18:52 +0100

    * Added text to the hackbox

--- Shadowbeta2/Assets/Scripts/DragAndDrop/DragAndDrop.cs
+++ Shadowbeta2/Assets/Scripts/DragAndDrop/DragAndDrop.cs
@@ -1,33 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
+    public GameObject defaultLocation;
+    public string GuessText;
+
     private bool dragging = false;
     private float distance;
+    private Transform defaultLocationTransform;
+    private Transform HackBoxTransform;
+    private TextMeshPro test;
 
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-
-    //}
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultLocationTransform = defaultLocation.GetComponent<Transform>();
+        HackBoxTransform = GetComponent<Transform>();
+        gameObject.transform.GetChild(0).gameObject.GetComponent<TextMeshPro>().text = GuessText;
+    }
 
     // Update is called once per frame
-    //void Update()
-    //{
-    //    Debug.Log(dragging);
-    //}
-
-    //private void OnMouseDown()
-    //{
-    //    dragging = true;
-    //}
-
-    //private void OnMouseUp()
-    //{
-    //    dragging = false;
-    //}
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint;
+        }
+        //TODO check if hackbox is in a emtpy location box, otherwise snap back to location
+        else if(!dragging)
+        {
+            HackBoxTransform.position = defaultLocationTransform.position;
+        }
+    }
 
     void OnMouseDown()
     {
@@ -40,13 +49,5 @@ public class DragAndDrop : MonoBehaviour
         dragging = false;
     }
 
-    void Update()
-    {
-        if (dragging)
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Vector3 rayPoint = ray.GetPoint(distance);
-            transform.position = rayPoint;
-        }
-    }
+    
 }
