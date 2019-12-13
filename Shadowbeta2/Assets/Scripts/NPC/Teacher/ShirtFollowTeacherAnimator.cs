99957973435ce6508252608ad4d9ebf14d38be02
Tomdatbenik <tomvankaathoven@gmail.com>
2019-12-13 11:36:54 +0100

    Teachers

---
+++ Shadowbeta2/Assets/Scripts/NPC/Teacher/ShirtFollowTeacherAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShirtFollowTeacherAnimator : MonoBehaviour
+{
+    public Animator teacher;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get animator of this component
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        animator.SetFloat("Horizontal", teacher.GetFloat("Horizontal"));
+        animator.SetFloat("Speed", teacher.GetFloat("Speed"));
+    }
+
+}
