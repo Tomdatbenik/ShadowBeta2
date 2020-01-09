0554a84c047a054edd6336c94a457f99dc2aeb28
wiebj3 <wiebj3@gmail.com>
2020-01-09 14:08:02 +0100

    hackeer pc screen added

--- Shadowbeta2/Assets/Scripts/GuessingPuzzel/InputPC.cs
+++ Shadowbeta2/Assets/Scripts/GuessingPuzzel/InputPC.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class InputPC : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TMP_InputField inputText;
+    int score = 0;
+    
+   
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            string input = inputText.text;
+            if(input.Contains("Jimmy"))
+            {
+                score++;
+            }
+            if(input.Contains("2018"))
+            {
+                score++;
+            }
+
+        }
     }
 }
