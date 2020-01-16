b6aa3a8381a39b74995553b7ef06f2730f4bb33e
Tomdatbenik <tomvankaathoven@gmail.com>
2020-01-16 13:28:32 +0100

    Start QuestList

---
+++ Shadowbeta2/Assets/Scripts/NPC/Teacher/setName.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class setName : MonoBehaviour
+{
+    public Conversation conversation;
+    public TextMeshPro tmp;
+
+    private void Start()
+    {
+        tmp.text = conversation.Talker;
+    }
+}
