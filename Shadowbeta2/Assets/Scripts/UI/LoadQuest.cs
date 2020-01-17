3fb6884897ef1641b386290a61e95f04e8e868b9
Tomdatbenik <tomvankaathoven@gmail.com>
2020-01-17 13:51:36 +0100

    QuestList

---
+++ Shadowbeta2/Assets/Scripts/UI/LoadQuest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadQuest : MonoBehaviour
+{
+    public Quest quest;
+
+    public Image mark; 
+    public TextMeshProUGUI text;
+
+    public Sprite HasQuest;
+    public Sprite AcceptedQuest;
+    public Sprite QuestComplete;
+    public Sprite EndedQuest;
+
+    private void Start()
+    {
+
+        text.text = quest.name;
+
+        switch (quest.QuestState)
+        {
+            case QuestState.COMPLETED:
+                mark.sprite = QuestComplete;
+                break;
+            case QuestState.ASSIGNED:
+                mark.sprite = AcceptedQuest;
+                break;
+            case QuestState.UNASSIGNED:
+                mark.sprite = HasQuest;
+                break;
+            case QuestState.ENDED:
+                mark.sprite = EndedQuest;
+                break;
+        }
+    }
+}
